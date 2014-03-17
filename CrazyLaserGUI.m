8ad565763b81aed2be1dcff88c4a5eeb3ffcad25
Beamanator <abeaman@mines.edu>
2014-03-17 10:34:20 -0600

    test

--- CrazyLaserGUI.m
+++ CrazyLaserGUI.m
@@ -74,7 +74,7 @@ function[] = CrazyLaserGUI
         %Parameters
         data.Parameters.ExampleValue = 0.0;
         data.Parameters.stepValue = 0.0;
-        data.Parameters.StepSize = 'EighthStep';
+        data.Parameters.StepSize = '1/8';
         
         %-----------------------------------------------------------------%
         %%%                            Panels        (Main_Figure)      %%%
